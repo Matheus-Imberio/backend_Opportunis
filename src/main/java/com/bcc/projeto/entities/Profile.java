b3114e139b256122d56e99e738632f478840b484
ruanazevedo <ruannzeroh@gmail.com>
2024-10-20 00:11:35 -0300

    classe Profile implementada

---
+++ src/main/java/com/bcc/projeto/entities/Profile.java
@@ -0,0 +1,62 @@
+package com.bcc.projeto.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+public abstract class Profile implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Getter
+	@Setter
+	private String name;
+	
+	@Getter
+	@Setter
+	private String email;
+	
+	@Getter
+	@Setter
+	private String telephone;
+	
+	@Getter
+	@Setter
+	private String password;
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, password, telephone);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Profile other = (Profile) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password) && Objects.equals(telephone, other.telephone);
+	}
+	
+	@Override
+	public String toString() {
+		return "Profile [id=" + id + ", name=" + name + ", email=" + email + ", telephone=" + telephone + ", password="
+				+ password + "]";
+	}	
+}
