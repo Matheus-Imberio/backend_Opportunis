53b7d2409af554ed81b59bc257f0345f98c6bf00
ruanazevedo <ruannzeroh@gmail.com>
2024-11-02 20:48:31 -0300

    CurriculumRepository implementado

--- src/main/java/com/bcc/projeto/repositories/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bcc.projeto.repositories;
-
-import com.bcc.projeto.entities.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
