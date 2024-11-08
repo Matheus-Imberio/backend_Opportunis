dbf13baf40203a7d9c5c8061d50b54dbca41902d
Matheus-Imberio <matheusimberio030@gmai.com>
2024-11-08 14:42:41 -0300

    Candidate controller created

--- src/main/java/com/bcc/projeto/controller/CandidateController.java
+++ src/main/java/com/bcc/projeto/controller/CandidateController.java
@@ -2,15 +2,12 @@ package com.bcc.projeto.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bcc.projeto.entities.Candidate;
@@ -30,11 +27,37 @@ public class CandidateController {
 		List<Candidate> allCandidates = candidateRepo.findAll();
 		return new ResponseEntity<List<Candidate>>(allCandidates, HttpStatus.OK);
 	}
-	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Candidate> findById(@PathVariable Long id) {
+		Optional<Candidate> obj = candidateRepo.findById(id);
+		if (obj.isPresent()) {
+			return ResponseEntity.ok(obj.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 	@PostMapping
 	public ResponseEntity<Candidate> insert(@RequestBody Candidate obj) {
 		obj = candidateRepo.save(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		if (candidateRepo.existsById(id)) {
+			candidateRepo.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Candidate> update(@PathVariable Long id, @RequestBody Candidate obj) {
+		if (!candidateRepo.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		obj.setId(id);
+		Candidate updatedCandidate = candidateRepo.save(obj);
+		return ResponseEntity.ok(updatedCandidate);
 	}
 }
